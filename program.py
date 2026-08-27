d73d05228df9370e35119a9ea226fd99edb7f49f
dimsam23 <dhimasdwi2311@students.undip.ac.id>
2026-08-27 16:43:58 +0700

    Add files via upload

---
+++ program.py
@@ -0,0 +1,7 @@
+import time
+name = input("What's your name? ")
+sport = input("What's your favorite sport? ")
+
+print ("Hello! " + name)
+time.sleep(1)
+print("So, your favorite sport is " + sport)
